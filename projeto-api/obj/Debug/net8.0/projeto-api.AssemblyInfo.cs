dd05199bc26c581dc045dc3b61e76b05379c68fa
manoellaborges <manoellabborges@gmail.com>
2024-09-19 16:50:28 -0300

    cria api

--- projeto-api/obj/Debug/net8.0/projeto-api.AssemblyInfo.cs
+++ projeto-api/obj/Debug/net8.0/projeto-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe3cd9c40c394b7c41f2c48de584366510453d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
